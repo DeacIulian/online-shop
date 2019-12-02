1f25c4578140d78b5ddaedb8b56e08190b39ac13
DeacIulian <deaciulian91@gmail.com>
2019-12-02 19:58:31 +0200

    studied custom queries in spring-boot repositories

--- src/main/java/org/fasttrackit/onlineshop/persistance/ProductRepository.java
+++ src/main/java/org/fasttrackit/onlineshop/persistance/ProductRepository.java
@@ -4,6 +4,7 @@ import org.fasttrackit.onlineshop.domain.Product;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
@@ -12,4 +13,10 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     Page<Product> findByNameContainingAndQuantityGreaterThanEqual(
             String partialName, int minQuantity, Pageable pageable);
 
+    //JPQL syntax (Java Persistence Query Language)
+//    @Query("SELECT product FROM Product product WHERE name LIKE '%:partialName%'")
+    //native MySQL query
+    //daca avem nume de coloane care coincid cu cuvintele cheie trebuie sa folosim tilda
+    @Query(value = "SELECT * FROM product WHERE `name` LIKE '%?0'", nativeQuery = true)
+    Page<Product> findByPartialName(String partialName, Pageable pageable);
 }
